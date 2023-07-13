50835b2a509c19d5316d42d1ea80468dbbda9008
csjames <csjames0303@gmail.com>
2023-07-13 15:58:53 +0800

    working on Schema

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,9 @@
-const app = require('./server');
-
+import app from './server';
 const port = 3000
 
 
 app.listen(port,async ()=>{
     console.log(`listening on port ${port}`)
-    const r = await  fetch('http://localhost:3000/name')
-    const response = await r.json()
-    console.log(response.name)
 })
 
 
