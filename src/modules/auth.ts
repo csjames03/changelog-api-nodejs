dfa120c79a2439f11d7fa170678b4c101bb1b187
csjames <csjames0303@gmail.com>
2023-07-13 23:04:28 +0800

    added token

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -9,3 +9,33 @@ export const createJWT = (user) =>{
         )
     return token
     }
+
+export const protect = (req, res, next) =>{
+     const bearer = req.headers.authorization
+
+
+     if(!bearer){
+        res.status(401)
+        res.json({ 'message': 'Not authorized' })
+        return
+     }
+
+     const [, token] =  bearer.split(" ")
+
+     if(!token){
+        res.status(401)
+        res.json({ 'message': 'Not valid Token' })
+        return
+     }
+
+    try{
+        const user = jwt.verify(token, process.env.JWT_SECRET)
+        req.user = user
+        next()
+     }catch(err){
+        console.log(err)
+        res.status(401)
+        res.json({ 'message': 'Not Valid Token' })
+        return
+     }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import router from './router';
 import morgan from 'morgan';
+import {protect} from './modules/auth';
 
 const app = express()
 
@@ -21,6 +22,6 @@ app.get('/', (req, res)=>{
     res.json({message: 'Hello from express'})
 })
 
-app.use('/api', router)
+app.use('/api',protect,  router)
 
 export default  app 
